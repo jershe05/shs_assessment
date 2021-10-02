8aa542645114d2a7d0439a97d25577591ebbc200
Jerome Palis <jershe05@gmail.com>
2021-10-02 21:43:18 +0800

    added registration

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -3,68 +3,64 @@
 @section('title', __('Login'))
 
 @section('content')
-    <div class="container py-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <x-frontend.card>
-                    <x-slot name="header">
-                        @lang('Login')
-                    </x-slot>
-
-                    <x-slot name="body">
-                        <x-forms.post :action="route('frontend.auth.login')">
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">@lang('E-mail Address')</label>
-
-                                <div class="col-md-6">
-                                    <input type="email" name="email" id="email" class="form-control" placeholder="{{ __('E-mail Address') }}" value="{{ old('email') }}" maxlength="255" required autofocus autocomplete="email" />
+    <!-- Outer Row -->
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">'Welcome Back!'</h1>
                                 </div>
-                            </div><!--form-group-->
+                                <form class="user">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user"
+                                        name="email" id="email" aria-describedby="emailHelp"
+                                            placeholder="{{ __('E-mail Address') }}" value="{{ old('email') }}" maxlength="255" required autofocus autocomplete="email">
+                                    </div>
+                                    <div class="form-group">
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">@lang('Password')</label>
+                                        <input type="password" class="form-control form-control-user"
+                                        name="password" id="password"
+                                        placeholder="{{ __('Password') }}"
+                                        maxlength="100" required autocomplete="current-password">
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input name="remember" id="remember" class="custom-control-input" type="checkbox" {{ old('remember') ? 'checked' : '' }} />
 
-                                <div class="col-md-6">
-                                    <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Password') }}" maxlength="100" required autocomplete="current-password" />
+                                            <label class="custom-control-label" for="remember">
+                                                @lang('Remember Me')
+                                            </label>
+
+                                        </div>
+                                    </div>
+                                    <button class="btn btn-primary btn-user btn-block" type="submit">@lang('Login')</button>
+
+                                </form>
+                                    @if(config('boilerplate.access.captcha.login'))
+                                        <div class="row">
+                                            <div class="col">
+                                                @captcha
+                                                <input type="hidden" name="captcha_status" value="true" />
+                                            </div><!--col-->
+                                        </div><!--row-->
+                                    @endif
+                                <hr>
+                                <div class="text-center">
+                                    <x-utils.link :href="route('frontend.auth.password.request')" class="small" :text="__('Forgot Your Password?')" />
                                 </div>
-                            </div><!--form-group-->
-
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input name="remember" id="remember" class="form-check-input" type="checkbox" {{ old('remember') ? 'checked' : '' }} />
-
-                                        <label class="form-check-label" for="remember">
-                                            @lang('Remember Me')
-                                        </label>
-                                    </div><!--form-check-->
+                                <div class="text-center">
+                                    <x-utils.link :href="route('frontend.auth.register')" class="small" :text="__('Register')" />
                                 </div>
-                            </div><!--form-group-->
-
-                            @if(config('boilerplate.access.captcha.login'))
-                                <div class="row">
-                                    <div class="col">
-                                        @captcha
-                                        <input type="hidden" name="captcha_status" value="true" />
-                                    </div><!--col-->
-                                </div><!--row-->
-                            @endif
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button class="btn btn-primary" type="submit">@lang('Login')</button>
-
-                                    <x-utils.link :href="route('frontend.auth.password.request')" class="btn btn-link" :text="__('Forgot Your Password?')" />
+                                <div class="text-center">
+                                    @include('frontend.auth.includes.social')
                                 </div>
-                            </div><!--form-group-->
-
-                            <div class="text-center">
-                                @include('frontend.auth.includes.social')
                             </div>
-                        </x-forms.post>
-                    </x-slot>
-                </x-frontend.card>
-            </div><!--col-md-8-->
-        </div><!--row-->
-    </div><!--container-->
+                        </div>
+                    </div>
+                </div>
+            </div>
 @endsection
--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -3,49 +3,36 @@
 @section('title', __('Register'))
 
 @section('content')
-    <div class="container py-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <x-frontend.card>
-                    <x-slot name="header">
-                        @lang('Register')
-                    </x-slot>
-
-                    <x-slot name="body">
-                        <x-forms.post :action="route('frontend.auth.register')">
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">@lang('Name')</label>
-
-                                <div class="col-md-6">
-                                    <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}" placeholder="{{ __('Name') }}" maxlength="100" required autofocus autocomplete="name" />
-                                </div>
-                            </div><!--form-group-->
-
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">@lang('E-mail Address')</label>
-
-                                <div class="col-md-6">
-                                    <input type="email" name="email" id="email" class="form-control" placeholder="{{ __('E-mail Address') }}" value="{{ old('email') }}" maxlength="255" required autocomplete="email" />
-                                </div>
-                            </div><!--form-group-->
-
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">@lang('Password')</label>
-
-                                <div class="col-md-6">
-                                    <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Password') }}" maxlength="100" required autocomplete="new-password" />
-                                </div>
-                            </div><!--form-group-->
-
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">@lang('Password Confirmation')</label>
-
-                                <div class="col-md-6">
-                                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="{{ __('Password Confirmation') }}" maxlength="100" required autocomplete="new-password" />
-                                </div>
-                            </div><!--form-group-->
-
-                            <div class="form-group row">
+<div class="card o-hidden border-0 shadow-lg my-5">
+    <div class="card-body p-0">
+        <!-- Nested Row within Card Body -->
+        <div class="row">
+            <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+            <div class="col-lg-7">
+                <div class="p-5">
+                    <div class="text-center">
+                        <h1 class="h4 text-gray-900 mb-4">@lang('Register')</h1>
+                    </div>
+                    <x-forms.post  class="user" :action="route('frontend.auth.register')">
+                        <div class="form-group">
+                            <input type="text" class="form-control form-control-user" name="name" id="name"
+                            value="{{ old('name') }}" placeholder="{{ __('Name') }}" maxlength="100" required autofocus autocomplete="name">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control form-control-user" name="email" id="email"
+                            placeholder="{{ __('E-mail Address') }}" value="{{ old('email') }}" maxlength="255" required autocomplete="email">
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                <input type="password" class="form-control form-control-user"
+                                name="password" id="password" placeholder="{{ __('Password') }}" maxlength="100" required autocomplete="new-password">
+                            </div>
+                            <div class="col-sm-6">
+                                <input type="password" class="form-control form-control-user"
+                                name="password_confirmation" id="password_confirmation" placeholder="{{ __('Password Confirmation') }}" maxlength="100" required autocomplete="new-password">
+                            </div>
+                        </div>
+                        <div class="form-group row">
                                 <div class="col-md-6 offset-md-4">
                                     <div class="form-check">
                                         <input type="checkbox" name="terms" value="1" id="terms" class="form-check-input" required>
@@ -54,8 +41,7 @@
                                         </label>
                                     </div>
                                 </div>
-                            </div><!--form-group-->
-
+                            </div>
                             @if(config('boilerplate.access.captcha.registration'))
                                 <div class="row">
                                     <div class="col">
@@ -64,16 +50,19 @@
                                     </div><!--col-->
                                 </div><!--row-->
                             @endif
+                        <button class="btn btn-primary btn-user btn-block" type="submit">@lang('Register')</button>
+                        <hr>
+                    </x-forms.post>
 
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button class="btn btn-primary" type="submit">@lang('Register')</button>
-                                </div>
-                            </div><!--form-group-->
-                        </x-forms.post>
-                    </x-slot>
-                </x-frontend.card>
-            </div><!--col-md-8-->
-        </div><!--row-->
-    </div><!--container-->
+                    <div class="text-center">
+                        <x-utils.link :href="route('frontend.auth.password.request')" class="small" :text="__('Forgot Your Password?')" />
+                    </div>
+                    <div class="text-center">
+                        <x-utils.link :href="route('frontend.auth.login')" class="small" :text="__('Already have an account? Login!')" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
